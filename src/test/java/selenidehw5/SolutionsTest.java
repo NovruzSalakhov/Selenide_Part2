37b57427b185371215243e83e4edb43d287295c7
NovruzSalakhov <salakhov-novruz@mail.ru>
2023-08-08 20:11:26 +0300

    added new Class for Test

---
+++ src/test/java/selenidehw5/SolutionsTest.java
@@ -0,0 +1,16 @@
+package selenidehw5;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+public class SolutionsTest {
+    @Test
+    void SelenideSolutionsEnterprise () {
+        open("https://github.com/");
+        $(".header-menu-wrapper").$(byText("Solutions")).hover();
+        $(".header-menu-wrapper").$(byText("Enterprise")).click();
+        $(".application-main  h1").shouldHave(text("Build like the best"));
+    }
+}
